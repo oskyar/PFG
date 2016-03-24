9b35d7e0b082c803121492af57079a7bfefe458c
oskyar <oskyar@gmail.com>
2016-03-24 19:47:16 +0100

    - Ahora se añade a la variable de sesión, la foto del usuario. Para esto se han tenido que crear señales que se ejecutan tanto al identificarse el usuario como desconectarse.

--- TFG/urls.py
+++ TFG/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout_then_login
 from django.views.static import serve
+import logging
+from importlib import import_module
+from django.conf import settings
 
 # from TFG.apps.user.forms import UserProfileForm
 # from registration.views import RegistrationView
@@ -27,3 +30,21 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,
         {'document_root': settings.MEDIA_ROOT,}),
 ]
+
+
+import_module("TFG.apps.index.signals")
+
+#Este código sirve para buscar todos los signals añadidos a las apps
+"""logger = logging.getLogger(__name__)
+
+signal_modules = {}
+
+for app in settings.INSTALLED_APPS:
+    signals_module = '%s.signals' % app
+    try:
+        logger.debug('loading "%s" ..' % signals_module)
+        signal_modules[app] = import_module(signals_module)
+    except ImportError as e:
+        logger.warning(
+            'failed to import "%s", reason: %s' % (signals_module, str(e)))
+"""
