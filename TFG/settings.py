884d44b822b9b1fce3c51c2d814405d04ef85c2a
oskyar <oskyar@gmail.com>
2016-06-30 14:12:25 +0200

    Probando para desplegar en heroku

--- TFG/settings.py
+++ TFG/settings.py
@@ -39,17 +39,12 @@ DATABASES = {
 
 # db_from_env = dj_database_url.config()
 DATABASES['default'].update(dj_database_url.config())
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
